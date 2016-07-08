0f7abc3c70eda6a003c32cdcb76d501974bbec4e
opw0011 <opw0011@gmail.com>
2016-07-08 14:42:40 +0800

    add route param

--- app/show-nav-path/show-nav-path.js
+++ app/show-nav-path/show-nav-path.js
@@ -3,12 +3,24 @@
 angular.module('myApp.show-nav-path', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/show-nav-path', {
+  $routeProvider.when('/show-nav-path/:action', {
     templateUrl: 'show-nav-path/show-nav-path.html',
     controller: 'ShowNavPathCtrl'
   });
 }])
 
-.controller('ShowNavPathCtrl', [function() {
+.controller('ShowNavPathCtrl', function($scope, $routeParams) {
+  console.log($routeParams.action);
+  $scope.action = $routeParams.action;
 
-}]);
+  // return url path after arrived
+  $scope.getLink = function() {
+    if($routeParams.action == "enter")
+      return "#!arrive-park-lot";
+    else if($routeParams.action == "exit")
+      return "#!exit-park-lot";
+    else
+      return "#!";
+  }
+
+});
